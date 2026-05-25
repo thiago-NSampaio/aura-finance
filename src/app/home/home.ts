aa2dac2313fa69edf004a349d5432d563a08f104
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2026-05-24 21:56:38 -0400

    create page init

---
+++ src/app/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SvgIconComponent } from 'angular-svg-icon';
+
+@Component({
+  selector: 'app-home',
+  imports: [SvgIconComponent],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+
+}
